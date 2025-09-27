0f9421446fd2ebbd0d77e418f1a27495178a7db1
Diego Machaca <me@codiego.dev>
2025-09-26 23:31:57 -0500

    fix: lspconfig setup update

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,60 +2,60 @@
 local M = {}
 
 function M.map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then options = vim.tbl_extend("force", options, opts) end
-  vim.keymap.set(mode, lhs, rhs, options)
+    local options = { noremap = true, silent = true }
+    if opts then options = vim.tbl_extend("force", options, opts) end
+    vim.keymap.set(mode, lhs, rhs, options)
 end
 
 function M.setup()
-  -- Archivo
-  M.map("n", "<leader>s", ":w<CR>")
-  M.map("n", "<leader>q", ":q<CR>")
-  M.map("n", "<leader>h", ":nohlsearch<CR>")
+    -- Archivo
+    M.map("n", "<leader>s", ":w<CR>")
+    M.map("n", "<leader>q", ":q<CR>")
+    M.map("n", "<leader>h", ":nohlsearch<CR>")
 
-  -- Salir de insert sin Esc
-  M.map("i", "jk", "<Esc>")
+    -- Salir de insert sin Esc
+    M.map("i", "jk", "<Esc>")
 
-  -- Format con LSP
-  M.map("n", "<leader>f", function()
-    vim.lsp.buf.format({ async = true })
-  end, { desc = "Format code" })
+    -- Format con LSP
+    M.map("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+    end, { desc = "Format code" })
 
-  -- Crear / cerrar splits
-  M.map("n", "<leader>v", ":vsplit<CR>", { desc = "Split vertical" })
-  M.map("n", "<leader>x", ":split<CR>",  { desc = "Split horizontal" })
-  M.map("n", "<leader>c", ":close<CR>",  { desc = "Cerrar split actual" })
+    -- Crear / cerrar splits
+    M.map("n", "<leader>v", ":vsplit<CR>", { desc = "Split vertical" })
+    M.map("n", "<leader>x", ":split<CR>", { desc = "Split horizontal" })
+    M.map("n", "<leader>c", ":close<CR>", { desc = "Cerrar split actual" })
 
-  -- Navegación entre splits
-  M.map("n", "<C-h>", "<C-w>h")
-  M.map("n", "<C-j>", "<C-w>j")
-  M.map("n", "<C-k>", "<C-w>k")
-  M.map("n", "<C-l>", "<C-w>l")
+    -- Navegación entre splits
+    M.map("n", "<C-h>", "<C-w>h")
+    M.map("n", "<C-j>", "<C-w>j")
+    M.map("n", "<C-k>", "<C-w>k")
+    M.map("n", "<C-l>", "<C-w>l")
 
-  -- Buffers
-  M.map("n", "<S-l>", ":bnext<CR>")
-  M.map("n", "<S-h>", ":bprevious<CR>")
-  M.map("n", "<leader>bd", ":bdelete<CR>")
+    -- Buffers
+    M.map("n", "<S-l>", ":bnext<CR>")
+    M.map("n", "<S-h>", ":bprevious<CR>")
+    M.map("n", "<leader>bd", ":bdelete<CR>")
 
-  -- Mover líneas en visual
-  M.map("v", "J", ":m '>+1<CR>gv=gv")
-  M.map("v", "K", ":m '<-2<CR>gv=gv")
+    -- Mover líneas en visual
+    M.map("v", "J", ":m '>+1<CR>gv=gv")
+    M.map("v", "K", ":m '<-2<CR>gv=gv")
 
-  -- Mantener selección al indentar
-  M.map("v", "<", "<gv")
-  M.map("v", ">", ">gv")
+    -- Mantener selección al indentar
+    M.map("v", "<", "<gv")
+    M.map("v", ">", ">gv")
 
-  -- Pegar sin sobrescribir el registro
-  M.map("v", "p", '"_dP')
+    -- Pegar sin sobrescribir el registro
+    M.map("v", "p", '"_dP')
 
-  -- Centrar al hacer scroll / buscar
-  M.map("n", "<C-d>", "<C-d>zz")
-  M.map("n", "<C-u>", "<C-u>zz")
-  M.map("n", "n", "nzzzv")
-  M.map("n", "N", "Nzzzv")
+    -- Centrar al hacer scroll / buscar
+    M.map("n", "<C-d>", "<C-d>zz")
+    M.map("n", "<C-u>", "<C-u>zz")
+    M.map("n", "n", "nzzzv")
+    M.map("n", "N", "Nzzzv")
 
-  -- Seleccionar todo
-  M.map("n", "<leader>a", "ggVG")
+    -- Seleccionar todo
+    M.map("n", "<leader>a", "ggVG")
 end
 
 return M
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,128 +1,128 @@
 return {
-  -- Explorador de archivos
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
-    keys = {
-      { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle file tree", silent = true },
-    },
-    config = function()
-      require("nvim-tree").setup()
-    end,
-  },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Telescope",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>",       desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>",        desc = "Live grep" },
-      { "gd",         "<cmd>Telescope lsp_definitions<cr>",  desc = "Definitions" },
-      { "gr",         "<cmd>Telescope lsp_references<cr>",   desc = "References" },
-      { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
-    },
-    config = function()
-      require("telescope").setup()
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "lua", "typescript", "javascript", "rust", "json", "markdown" },
-        highlight = { enable = true }
-      }
-    end
-  },
-
-  -- Statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    config = function()
-      require("lualine").setup {
-        options = {
-          theme = "catppuccin",
-          component_separators = { left = "│", right = "│" },
-          section_separators = { left = "", right = "" },
-          globalstatus = true,
+    -- Explorador de archivos
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
+        keys = {
+            { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle file tree", silent = true },
         },
-        sections = {
-          lualine_b = { "branch", "diff" },
-          lualine_c = { { "filename", path = 1 } },
-          lualine_x = {
-            {
-              "diagnostics",
-              sources = { "nvim_diagnostic" },
-              symbols = { error = " ", warn = " ", info = " ", hint = " " },
-            },
-            "encoding",
-            "filetype",
-          },
-        },
-      }
-    end
-  },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      background = { light = "latte", dark = "mocha" },
-      blur = {
-        enabled = true,
-        timeout = 100,
-      },
-      integrations = {
-        treesitter = true,
-        lualine = true,
-        mason = true,
-        telescope = true,
-        nvimtree = true,
-        which_key = true,
-        bufferline = true,
-        gitsigns = true,
-        mini = { enabled = true },
-      },
-      transparent_background = true,
+        config = function()
+            require("nvim-tree").setup()
+        end,
     },
-  },
 
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonUninstall" },
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "ts_ls", "lua_ls" },
-        automatic_enable = { exclude = { "stylua" } },
-      }
-      vim.lsp.config("ts_ls", {})
-      vim.lsp.config("lua_ls", {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = "Telescope",
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>",          desc = "Find files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>",           desc = "Live grep" },
+            { "gd",         "<cmd>Telescope lsp_definitions<cr>",     desc = "Definitions" },
+            { "gr",         "<cmd>Telescope lsp_references<cr>",      desc = "References" },
+            { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
+        },
+        config = function()
+            require("telescope").setup()
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "lua", "typescript", "javascript", "rust", "json", "markdown" },
+                highlight = { enable = true }
             }
-          }
-        }
-      })
-      vim.lsp.enable({ "ts_ls", "lua_ls" })
-    end
-  }
+        end
+    },
+
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        event = "VeryLazy",
+        config = function()
+            require("lualine").setup {
+                options = {
+                    theme = "catppuccin",
+                    component_separators = { left = "│", right = "│" },
+                    section_separators = { left = "", right = "" },
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_b = { "branch", "diff" },
+                    lualine_c = { { "filename", path = 1 } },
+                    lualine_x = {
+                        {
+                            "diagnostics",
+                            sources = { "nvim_diagnostic" },
+                            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+                        },
+                        "encoding",
+                        "filetype",
+                    },
+                },
+            }
+        end
+    },
+
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            background = { light = "latte", dark = "mocha" },
+            blur = {
+                enabled = true,
+                timeout = 100,
+            },
+            integrations = {
+                treesitter = true,
+                lualine = true,
+                mason = true,
+                telescope = true,
+                nvimtree = true,
+                which_key = true,
+                bufferline = true,
+                gitsigns = true,
+                mini = { enabled = true },
+            },
+            transparent_background = true,
+        },
+    },
+
+    {
+        "williamboman/mason.nvim",
+        cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonUninstall" },
+        config = true,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "ts_ls", "lua_ls" },
+                automatic_enable = { exclude = { "stylua" } },
+            }
+            vim.lsp.config("ts_ls", {})
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" }
+                        }
+                    }
+                }
+            })
+            vim.lsp.enable({ "ts_ls", "lua_ls" })
+        end
+    }
 
 }
