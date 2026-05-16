255792e69e96d5ffbc82536af751705026ba9e98
Diego <diegomachaca@ihfintech.com.pe>
2026-05-16 00:12:58 -0500

    chore(nvim): some fixes and improvenments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,29 +1,38 @@
 return {
-  'folke/lazy.nvim',
+  -- Explorador de archivos
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = { "NvimTreeToggle", "NvimTreeFocus", "NvimTreeFindFile" },
+    keys = {
+      { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle file tree", silent = true },
+    },
     config = function()
       require("nvim-tree").setup()
-      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-    end
+    end,
   },
 
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",       desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",        desc = "Live grep" },
+      { "gd",         "<cmd>Telescope lsp_definitions<cr>",  desc = "Definitions" },
+      { "gr",         "<cmd>Telescope lsp_references<cr>",   desc = "References" },
+      { "gi",         "<cmd>Telescope lsp_implementations<cr>", desc = "Implementations" },
+    },
     config = function()
-      local telescope = require("telescope")
-      telescope.setup()
-      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-    end
+      require("telescope").setup()
+    end,
   },
 
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = { "lua", "typescript", "javascript", "rust", "json", "markdown" },
@@ -36,8 +45,29 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     config = function()
-      require("lualine").setup { options = { theme = "gruvbox" } }
+      require("lualine").setup {
+        options = {
+          theme = "catppuccin",
+          component_separators = { left = "│", right = "│" },
+          section_separators = { left = "", right = "" },
+          globalstatus = true,
+        },
+        sections = {
+          lualine_b = { "branch", "diff" },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = {
+            {
+              "diagnostics",
+              sources = { "nvim_diagnostic" },
+              symbols = { error = " ", warn = " ", info = " ", hint = " " },
+            },
+            "encoding",
+            "filetype",
+          },
+        },
+      }
     end
   },
 
@@ -59,6 +89,9 @@ return {
         telescope = true,
         nvimtree = true,
         which_key = true,
+        bufferline = true,
+        gitsigns = true,
+        mini = { enabled = true },
       },
       transparent_background = true,
     },
@@ -66,22 +99,20 @@ return {
 
   {
     "williamboman/mason.nvim",
-    config = true
+    cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonUninstall" },
+    config = true,
   },
-  {
-    "neovim/nvim-lspconfig"
-  },
-
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
+    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       require("mason-lspconfig").setup {
-        ensure_installed = { "ts_ls", "lua_ls" }
+        ensure_installed = { "ts_ls", "lua_ls" },
+        automatic_enable = { exclude = { "stylua" } },
       }
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup {}
-      lspconfig.lua_ls.setup {
+      vim.lsp.config("ts_ls", {})
+      vim.lsp.config("lua_ls", {
         settings = {
           Lua = {
             diagnostics = {
@@ -89,7 +120,8 @@ return {
             }
           }
         }
-      }
+      })
+      vim.lsp.enable({ "ts_ls", "lua_ls" })
     end
   }
 
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 
-function scheme_for_appearance(appearance)
-  if appearance:find "Dark" then
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
     return "Catppuccin Mocha"
   else
     return "Catppuccin Latte"
@@ -9,63 +9,62 @@ function scheme_for_appearance(appearance)
 end
 
 local config = wezterm.config_builder()
-local scheme = wezterm.get_builtin_color_schemes()['Catppuccin Mocha']
+
+local scheme = wezterm.get_builtin_color_schemes()["Catppuccin Mocha"]
 scheme.background = "black"
 config.color_schemes = {
-  ['Catppuccin Mocha'] = scheme,
+  ["Catppuccin Mocha"] = scheme,
 }
-config.font = wezterm.font("CommitMono Nerd Font Mono")
-config.font_size = 16
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
 
+config.font = wezterm.font("CommitMono Nerd Font Mono")
+config.font_size = 14
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = false
+config.window_decorations = "RESIZE"
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
-
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
+config.scrollback_lines = 10000
+config.window_padding = { left = 4, right = 4, top = 4, bottom = 0 }
+config.max_fps = 120
+config.automatically_reload_config = true
+config.audible_bell = "Disabled"
+config.adjust_window_size_when_changing_font_size = false
 
-config.keys = {
-  {
-    key = 'd',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
-  {
-    key = 'h',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'l',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'k',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+table.insert(config.hyperlink_rules, {
+  regex = [[\bgh:([\w-]+)/([\w.-]+)#(\d+)\b]],
+  format = "https://github.com/$1/$2/issues/$3",
+})
+
+wezterm.on("window-config-reloaded", function(window)
+  local overrides = window:get_config_overrides() or {}
+  local next_scheme = scheme_for_appearance(window:get_appearance())
+  if overrides.color_scheme ~= next_scheme then
+    overrides.color_scheme = next_scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
+local keys = {
+  { key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "d", mods = "CMD",       action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "w", mods = "CMD",       action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 }
-
+for key, dir in pairs({ h = "Left", l = "Right", k = "Up", j = "Down" }) do
+  table.insert(keys, {
+    key = key,
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivatePaneDirection(dir),
+  })
+end
+config.keys = keys
 
 return config
